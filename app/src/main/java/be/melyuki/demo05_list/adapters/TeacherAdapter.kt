069280a3b1f511b022713f221b9f954f3b967cf2
melvin taider <tmneo1989@hotmail.com>
2023-07-31 09:55:32 +0200

    Comment_update

--- app/src/main/java/be/melyuki/demo05_list/adapters/TeacherAdapter.kt
+++ app/src/main/java/be/melyuki/demo05_list/adapters/TeacherAdapter.kt
@@ -13,28 +13,36 @@ class TeacherAdapter(val Context : Context, val Teachers : List<Teacher> ) : Rec
 
     class TeacherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        // Encapsule la view d'un item de la liste
+        // - Permet de récup les différents éléments de la view
+
         val TvFirstName : TextView
         val TvLastName : TextView
-
         init {
+            // Récup des éléments via la méthode "findViewById" depuis la view reçue
             TvFirstName = itemView.findViewById(R.id.item_teacher_firstname)
             TvLastName = itemView.findViewById(R.id.item_teacher_lastname)
         }
     }
 
+    // La fait d'hériter de "RecyclerView.Adapter" oblige l'override de ces 3 méthodes
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeacherViewHolder {
+        // La méthode appelée crée une view encapsulée dans un ViewHolder
         val v : View = LayoutInflater.from(Context).inflate(R.layout.item_teacher, parent, false)
         return TeacherViewHolder(v)
     }
 
-    override fun getItemCount(): Int {
-        return Teachers.size
-    }
-
     override fun onBindViewHolder(holder: TeacherViewHolder, position: Int) {
+        // Méthode appelée pour remplir le contenu de la view en fonction de la position dans la liste
         val target : Teacher = Teachers[position]
 
         holder.TvFirstName.setText(target.FirstName)
         holder.TvLastName.setText(target.LastName)
     }
+
+    override fun getItemCount(): Int {
+        // Méthode appelée pour obtenir le nombre d'élément dans la liste (nécessaire pour le visuel)
+        return Teachers.size
+    }
+
 }
